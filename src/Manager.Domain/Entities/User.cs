c76951de45a8ca6680dee0d2a157ad9fa181d26e
LeonarDev <leonardo.majevski@gmail.com>
2021-11-08 23:32:51 -0300

    Implementa arquitetura básica, Classe Base, Classe User e validações

---
+++ src/Manager.Domain/Entities/User.cs
@@ -0,0 +1,50 @@
+namespace Manager.Domain.Entities
+{
+    public class User : Base
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        protected User(){ } //EF
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            _errors = new List<string>();
+        }
+
+        public void ChangeName(string name){
+            Name = name;
+            Validate();
+        }
+
+        public void ChangePasword(string password){
+            Password = password;
+            Validate();
+        }
+
+        public void ChangeEmail(string email){
+            Email = email;
+            Validate();
+        }
+
+        public override bool Validate()
+        {
+            var validator = new UserValidator();
+            var validation = validator.Validate(this);
+
+            if(!validation.IsValid)
+            {
+                foreach(var error in validation.Errors)
+                {
+                    _errors.Add(error.ErrorMessage);
+
+                    throw new System.Exception("Existe algum campo inválido.", _errors[0]);
+                }
+            }
+        }
+    }
+}
